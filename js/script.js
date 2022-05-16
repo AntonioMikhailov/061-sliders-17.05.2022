c556f0d5a09595df11a7c3529182af13a7077029
Anton <antoniom@mail.ru>
2022-05-16 19:22:13 +0300

    16.05-Json-server-Fetch-send-dataFromForm-to db.json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 window.addEventListener('DOMContentLoaded', ()=> {
-  
 const tabs = document.querySelectorAll('.tabheader__item');
 const tabsContent = document.querySelectorAll('.tabcontent');
 const tabsParent = document.querySelector('.tabheader__items');
-
 //функция скрытия табов  и класса выделения текста
 function hideTabContent() {
   tabsContent.forEach(item => { 
@@ -14,21 +12,18 @@ function hideTabContent() {
     });
  }
 hideTabContent();
-
 // ф. показа табов
 function showTabContent(i=0) { 
   tabsContent[i].style.display = 'block';
   tabs[i].classList.add('tabheader__item_active');
 }
 showTabContent(); // показали первый таб при загрузке страницы
-
 // создаем обработчик для показа нужного Таба
 tabsParent.addEventListener('click', (e)=> {
   //для e.target создаем переменную
   const target = e.target;
   if(target.classList.contains('tabheader__item')) {
     tabs.forEach((item, i) => { 
-     
       if(target == item) {
         hideTabContent();
         showTabContent(i);
@@ -43,19 +38,13 @@ tabsParent.addEventListener('click', (e)=> {
 //     showTabContent(i);
 //   }); 
 //  });
-
-
 // Таймер обратного отсчета времени Акции
-
 // создаем дату конца акции
 let deadLine = '2022-07-17T00:00:00+0300'; // так добавляет к текущему времени 3 часа
-
-
 // Функ. которая будет определять разницу между deadline и текущим временем временем
 function getTimerRemaining (endtime) { 
   let t = Date.parse(endtime) - Date.parse(new Date()); // получаем разницу в милисекундах между концом и текущим
   // console.log(t);
- 
   // Теперь нам надо получить из милисекунд дни, часы, мин и сек.
   let days = Math.floor( t / (24*3600*1000)); //  у Кантора см. расчет мой
   // console.log(days); // 10
@@ -66,7 +55,6 @@ function getTimerRemaining (endtime) {
 // console.log(minutes);
  let seconds = Math.floor(t / (1000) % 60);
   // console.log(seconds);
-
   //выводим данные наружу в виде объекта 
   return {
 'total': t,
@@ -74,20 +62,16 @@ function getTimerRemaining (endtime) {
 'hours': hours,
 'minutes': minutes,
 'seconds': seconds,
-
   };
 }
 //функция добавления нулей перед числами до 10
 function getZero(num) { 
   if(num > 0 && num <10) {
     return `0${num}`;
-
   } else {
     return num;
   } 
- 
 }
-
 //Создаем ф. которая устанавливает таймер на страницу
 function setClock(selector, endtime) { 
   //передаем класс timer в виде переменной
@@ -98,12 +82,10 @@ function setClock(selector, endtime) {
   const minutes = timer.querySelector('#minutes');
   const seconds = timer.querySelector('#seconds');
   let timeInterval = setInterval(updateClock, 1000);
-
   //внутри ф. создаем ф. которая будет обновлять данные часов
   function updateClock() { 
     // получаем объект из  
    let t = getTimerRemaining(endtime);
-
    days.innerHTML = getZero(t.days);
    hours.innerHTML = getZero(t.hours);
    minutes.innerHTML = getZero(t.minutes);
@@ -112,19 +94,13 @@ function setClock(selector, endtime) {
    if (t.total <= 0) {
      clearInterval(timeInterval);
    }
-
-   
   }
-
 }
 setClock('.timer', deadLine ); // deadline далее в параметрах как endtime везде
-
-
 // ++++ Модальные окна ++++++++++++++++++++++++++++++
 const modal = document.querySelector('.modal');
 const btnOpenModal = document.querySelectorAll('[data-modal]');
 // const btnCloseModal = document.querySelector('[data-close]');
-
 // Суть - автопоказ по скролу и по таймеру всего один раз при загрузке страницы. Если до этого автопоказа юзер сам вызвал модалку то автопоказ убирается навсегда. И потом юзер может только вручную вызывать модалку
 //ф. открытиф модалки
 function openModal() { 
@@ -136,15 +112,12 @@ function openModal() {
   window.removeEventListener('scroll', showModalByScroll);
   // clearTimeout(showModalDelay);
 }
-
 // ф. закрытия модалки
 function closeModal() { 
   modal.style.display = 'none';
      document.body.style.overflow = '';
      document.body.style.paddingRight = 0 + 'px';
-   
      }
-
 btnOpenModal.forEach(item => { 
   item.addEventListener('click', openModal);
   // или так
@@ -153,9 +126,7 @@ btnOpenModal.forEach(item => {
   // }); 
     // также если юзер сам кликнет по модалке то убираем ее показ с задержкой или промоткой до низа
     window.removeEventListener('scroll', showModalByScroll);
-   
  });
-
  // закрываем с помощью делегирования + event target
 //  modal.addEventListener('click', (e)=> { 
 //    if(e.target == btnCloseModal|| e.target == modal ) {
@@ -163,7 +134,6 @@ btnOpenModal.forEach(item => {
 //     document.body.style.overflow = '';
 //     document.body.style.paddingRight = 0 + 'px';
 //    }
-  
 //  });
  // закрываем по Esc
 //  window.addEventListener('keydown', (e)=> { 
@@ -172,7 +142,6 @@ btnOpenModal.forEach(item => {
 //     document.body.style.overflow = '';
 //     document.body.style.paddingRight = 0 + 'px';
 //    }
-   
 //  });
 // Объединяем два события в одном слушателе
  ['click', 'keydown'].forEach(function(item) {
@@ -183,13 +152,10 @@ btnOpenModal.forEach(item => {
       closeModal();
     }
  });
- 
  });
-
   // Появление модалки при скроле до низа ( минус 300px) или после 10 секунд с начала входа на сайт
   // Для этого создадим функциии открытия модалки и закрытия
   let showModalDelay = setTimeout( openModal, 50000);
-
 // ф. показа при скроле вниз
 function showModalByScroll() { 
   let scrollHeight = document.body.scrollHeight; // вся высота body
@@ -201,15 +167,11 @@ function showModalByScroll() {
       openModal();
 }
 }
-
   window.addEventListener('scroll', showModalByScroll);
-
 // console.log(document.body.scrollHeight);
 // console.log(document.documentElement.scrollTop);
 // console.log(window.pageYOffset);
-
 // console.log(document.documentElement.clientHeight);
-
 // Классы для карточек урок 48
 // Что нужно для карточки - путь к фото, алт.текст для фото, заголовок, описание, цена в долларах
 // Создаем те свойства которые будут меняться с сервера. Размеры будут те же. Класс будет создавать только верстку
@@ -232,15 +194,12 @@ this.price = this.price * this.transfer;
     const element = document.createElement('div');
     // если доп. классы не передадутся мы сами их добавим
     if(this.classes.length ==0) {element.classList.add('menu__item');} 
-   
-   
     // добавляем класс селектор через оператор rest ( classes) - их может быть несколько  - поэтому через цикл
     this.classes.forEach(itemClass => { 
       element.classList.add(itemClass); 
      });
     // внутрь пустого div вставляем образец верстки из HTML
     element.innerHTML = `
-  
     <img src=${this.src} alt=${this.alt}>
     <h3 class="menu__item-subtitle">${this.title}</h3>
     <div class="menu__item-descr">${this.descr}</div>
@@ -249,13 +208,10 @@ this.price = this.price * this.transfer;
         <div class="menu__item-cost">Цена:</div>
         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
     </div>
-
     `;
     this.parent.append(element); // вставляем элемент внутрь родителя в конец
-
   }
 }
-
 // Шаблон класса готов - создаем новые объекты
 // 1 вариант
 let div = new MenuCard( // передаем аргументы через запятую
@@ -265,8 +221,6 @@ let div = new MenuCard( // передаем аргументы через зап
 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 12,
 '.menu .container',
-
-
 );
 div.render();
 // console.log(div);
@@ -279,9 +233,7 @@ new MenuCard(
 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
 7,
 '.menu .container',
-
 ).render();
-
 new MenuCard(
   "img/tabs/post.jpg",
 "йоги",
@@ -292,8 +244,8 @@ new MenuCard(
 'menu__item'
 ).render();
 
-// Отправка Форм обратной связи через AJAX на локальный файл server.php
 
+// Отправка Форм обратной связи через AJAX на локальный файл server.php
 // Находим все формы по тегу form 3шт
 const forms = document.querySelectorAll('form');
 //кнопку в переменную не нужно т.к. form отправляет автоматом если кнликнуть по тегу button
@@ -303,16 +255,28 @@ const message =  {
   success: 'Спасибо! Скоро мы с Вами свяжемся',
   failed: 'Что то пошло не так...'
 };
-
 //привязываем поля ввода к formData
 forms.forEach(item => { 
-  postData(item);  //вызываем функцию
+  bindpostData(item);  //вызываем функцию
  });
 
-function postData(form) {
+// создаем отдельную ф. по общению с сервером
+// Важно сто код здесь асинхронный и мы не знаем сколько времени на ответ от сервера уйдет и код не ждет выполнения fetch() а идет дальше поэтому используем async/await ( всегда в паре)
+const postData = async (url, data)=> {
+  const res = await fetch(url, { // await - значит будет ждать результат запроса и только потом продолжит выполнение
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+   body : data
+
+  });
+  return await res.json(); // возвращает Promise дальше в then - также нужен await
+};
+
+function bindpostData(form) {
   form.addEventListener('submit', (e)=> {
     e.preventDefault();  //отменяем перезагрузку
-
     // создаем новый блок для spinner
   let statusMessage = document.createElement('img');
   statusMessage.src = message.loading;
@@ -326,81 +290,32 @@ form.insertAdjacentElement('afterend', statusMessage );
     // Создаем метод отправки вариант XML Request - устаревший
     // const request = new XMLHttpRequest();
     // request.open('POST', 'server.php'); // метод и путь
-
 const formData = new FormData(form); //получаем данные из input
+// можно так получить формат JSON из FormData
+// const object = {};
+// formData.forEach(function(value, key) { 
+//   object[key] = value;
+//  });
+// или так - сначала превращаем в массив массивов а потом обратно в объект JSON
+const json = JSON.stringify(Object.fromEntries(formData.entries()));
 // Применяем метод Fetch
-fetch('server.php', {
-  method: 'POST',
-  // headers: {  // для formData заголоуки не нужны НО будут нужны для формата отправки JSON
-  //   'Content-type': 'application/json'
-  // },
-  body : formData
 
-}).then(data => data.text())
+postData('http://localhost:3000/requests', json) // server.php уже не нужен если Json сервер
+
 .then(data => {
-  console.log(data); // те данные который возвращаются из Promise
+  console.log(data); // те данные который возвращаются из Promise {"name":"Антон","phone":"+7911 291 0063"}
      //уведомляем юзера об успешной отправке его message
      showThanksModal(message.success);
        // После отправки формы очищаем поля и убираем надпись уведомление
-     
        setTimeout(() => {
         statusMessage.remove();
-       
         }, 2000);
 }).catch(()=> { // сюда можно добавить еще then(), но  мы пропишем блок catch() если будет ошибка от ответа сервера
   showThanksModal(message.failed);
 }).finally(()=> { // добавляем еще действие очистки формы - независимо от того как ответил сервер
   form.reset();
 }); 
-// для перевода в JSON создаем пустой объект, перебирем formData  с циклом forEach - Пример - но оставил formData
-// const object = {};
-// formData.forEach(function(value, key) { 
-//   object[key] = value;
-//  });
-
- // создаем доп. переменную
-//  const json = JSON.stringify(object); // перевели object  в JSON и теперь нужно  его поместить в  request.send(json)
-
-    // получаем данные из полей. Можно долгое решение - через сбор value из всех input. НО быстрее через встроен. объект formData
-//  request.send(json);
-// request.send(formData);
- //отслеживаем загрузку и ответ сервера
-//  request.addEventListener('load', ()=> { 
-//    if(request.status == 200) {
-//      console.log(request.response);
-//    //уведомляем юзера об успешной отправке его message
-//    showThanksModal(message.success);
-//      // После отправки формы очищаем поля и убираем надпись уведомление
-//      form.reset();
-//      setTimeout(() => {
-//       statusMessage.remove();
-     
-//       }, 2000);
-
-//    } else { // если не отправилось
-//     showThanksModal(message.failed);
-//    }
- 
-// });
-   // Тестируем формы на сервере и получаем пока что пустые массивы. Важно! Когда мы работаем в связке XMLHTTPRequest  -FormData - нам НЕ нужны заголовки. Они установятся автоматически
-   // В браузере Nerwork - находим файл server.php  - Headers  и смотрим процесс передачи 16-18
-   //  При отправке форм получаем такие данные  console.log(request.response);
-  //  array(2) {
-  //   ["name"]=>
-  //   string(6) "цыц"
-  //   ["phone"]=>
-  //   string(4) "3434"
-  // }
-  // Увидеть эти данные можно в Network - server.php - Payload или Preview, Response
-
-  // Если в формате Json передаем то ответ  console.log(request.response);
-  // console.log(json); //{"name":"axa","phone":"3434"}
- 
-
-
   });
-
- 
 } // конец ф. PostData
 
 // Создаем окно благодарности после отправки формы вместо простых надписей. Будем использовать блок modal__dialog и внего вставлять новый контент
@@ -412,57 +327,27 @@ function showThanksModal(message) {
  // создаем новый контент в окне
  let thanksModal = document.createElement('div');
  thanksModal.classList.add('modal__dialog');
-
 thanksModal.innerHTML = `
 <div class= 'modal__content'>
 <div class='modal__close' data-close>&times;</div>
 <div class='modal-title'>${message}</div> 
 </div>  
 `;
-
 // вставляем в верстку
 document.querySelector('.modal').append(thanksModal);
 setTimeout(() => {
   prevModalDialog.classList.remove('hide');
   thanksModal.remove();
-
   closeModal();
-
 }, 4000);
-
- 
 }
 
-// 054 - FETCH
-// fetch()
-// fetch('https://belarusbank.by/api/kursExchange')
-//   .then((response) => response.json())
-//     .then((json) => {
-//       console.log(json); 
-//     });
 
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method : 'POST',
-//   body: JSON.stringify({name: 'Alex'}), // данный которые отправляем
-//   headers: { // также добавляем заголовки
-//     'Content-type': 'application/json',
-//   }
-// })
-// .then((response) => response.json())
-//   .then((json) => {
-//     console.log(json); // получили ответ с нашими данными {name: 'Alex', id: 101}
-//  });
+// 058 - JSON сервер отправка в db.json
+// fetch('https://tochka-focusa.ru/111/db.json')
+// .then((data)=> data.json())
+// .then(res => console.log(res));
 
-// fetch('https://belarusbank.by/api/kursExchange', { mode: 'no-cors'})
-//   .then(blob => blob.json())
-//   .then(data => {
-//     console.table(data);
-//     return data;
-//   })
-//   .catch(e => {
-//     console.log(e);
-//     return e;
-//   });
 
 
 
